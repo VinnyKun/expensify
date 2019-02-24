0d9ae84a8183f3b0ad0b853dbec58b619aa8a2c3
vineethkunnathsg@gmail.com <vineethkunnathsg@gmail.com>
2019-02-24 22:05:47 +0800

    higher order component in playground

---
+++ src/playground/hoc.js
@@ -0,0 +1,38 @@
+//Just a component which renders another component; It is to reuse code
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Info = (props) => (
+    <div>
+        <h1>Info</h1>
+        <p>The info is: {props.info}</p>
+    </div>
+);
+
+const withAdminWarning = (WrappedComponent) => {
+    return (props) => (
+        <div>
+           {props.isAdmin && <p>This is private info. Please don't share.</p>}
+            <WrappedComponent {...props}/>
+        </div>
+    );
+}
+
+const requireAuthentication = (WrappedComponent) => {
+    return (props) => (
+        <div>
+            {props.isAuthenticated? (
+               <WrappedComponent {...props}/> 
+            ) : (
+                <p>Pleaselogin to view the info</p>
+            )}
+        </div>
+    )
+}
+
+const AdminInfo = withAdminWarning(Info);
+const AuthInfo = requireAuthentication(Info);
+
+//ReactDOM.render(<AdminInfo isAdmin={true} info='This is the info prop being passed down'/>, document.getElementById('app'));
+ReactDOM.render(<AuthInfo isAuthenticated={true} info='This is the info prop being passed down'/>, document.getElementById('app'));
